7d748078d854c3261d23a47b64ad2e7590238912
baboulinet <vergilotaku@live.fr>
2016-03-01 13:42:25 +0100

    fixed CheckCollision

--- SDLMarioBros/util.cpp
+++ SDLMarioBros/util.cpp
@@ -2,14 +2,14 @@
 
 bool CheckCollision(SDL_Rect a, SDL_Rect b)
 {
-	int a_l = a.x * a.w;
+	int a_l = a.x;
 	int a_r = a_l + a.w;
-	int a_t = a.y*a.h;
+	int a_t = a.y;
 	int a_b = a_t + a.h;
 
-	int b_l = b.x * b.w;
+	int b_l = b.x;
 	int b_r = b_l + b.w;
-	int b_t = b.y * b.h;
+	int b_t = b.y;
 	int b_b = b_t + b.h;
 
 	if (b_b <= a_t)
